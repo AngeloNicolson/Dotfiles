16e063b589b23d6eb614787440a313ed59fcad1a
angelonicolson <angelonicolson@outlook.com>
2025-10-10 20:14:52 +1300

    Configure noice.nvim to show messages in bottom right mini view

--- .config/nvim/lua/plugins/popup_and_ui.lua
+++ .config/nvim/lua/plugins/popup_and_ui.lua
@@ -8,6 +8,14 @@ return {
 		dependencies = { "rcarriga/nvim-notify" },
 		config = function()
 			require("noice").setup({
+				cmdline = {
+					enabled = true,
+					view = "cmdline_popup",
+				},
+				messages = {
+					enabled = true,
+					view = "mini", -- Use mini view for messages (bottom right corner)
+				},
 				-- Views for cmdline popup and popup menu
 				views = {
 					-- Cmdline popup configuration
@@ -40,6 +48,44 @@ return {
 						win_options = {
 							winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
 						},
+					},
+
+					-- Mini view for messages (bottom right, compact)
+					mini = {
+						backend = "mini",
+						relative = "editor",
+						align = "message-right",
+						timeout = 2000,
+						reverse = true,
+						focusable = false,
+						position = {
+							row = -2,
+							col = "100%",
+						},
+						size = "auto",
+						border = {
+							style = "none",
+						},
+						zindex = 60,
+						win_options = {
+							winblend = 0,
+							winhighlight = {
+								Normal = "NoiceMini",
+								IncSearch = "",
+								CurSearch = "",
+								Search = "",
+							},
+						},
+					},
+				},
+				routes = {
+					{
+						filter = {
+							event = "msg_show",
+							kind = "",
+							find = "written",
+						},
+						opts = { skip = true },
 					},
 				},
 			})
