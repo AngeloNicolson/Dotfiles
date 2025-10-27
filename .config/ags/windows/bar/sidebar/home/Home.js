9072f7178703940711fda81a9cda4b37e7719c88
angelonicolson <angelonicolson@outlook.com>
2025-10-27 14:24:36 +1300

    Add time card component to home pane

--- .config/ags/windows/bar/sidebar/home/Home.js
+++ .config/ags/windows/bar/sidebar/home/Home.js
@@ -1,5 +1,6 @@
 import UserHeader from './sections/UserHeader.js'
 import DesktopControls from './sections/DesktopControls.js'
+import TimeCard from './sections/TimeCard.js'
 import MusicPlayer from './sections/MusicPlayer.js'
 import NotificationCenter from './sections/NotificationCenter.js'
 
@@ -10,6 +11,7 @@ export default function() {
     children: [
       UserHeader(),
       DesktopControls(),
+      TimeCard(),
       MusicPlayer(),
       NotificationCenter()
     ]
